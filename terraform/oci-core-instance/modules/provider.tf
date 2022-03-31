ae36d0e96a26840184041191eac27a9154776aed
sureshvenkey <50266914+sureshvenkey@users.noreply.github.com>
2022-03-31 15:07:08 +0530

    Update provider.tf

--- terraform/oci-core-instance/modules/provider.tf
+++ terraform/oci-core-instance/modules/provider.tf
@@ -1,7 +1,7 @@
 provider "oci" {
   tenancy_ocid     = "ocid1.tenancy.oc1..aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"
   user_ocid        = "ocid1.user.oc1..aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"
-  fingerprint      = "aa:bb:cc:21:dd:ee:a7:da:33:79:ee:e5:7e:rr:b7:23"
+  fingerprint      = "aa:bb:cc:22:dd:ee:34:da:33:79:ee:e5:7e:rr:b7:23"
   region           = "uk-london-1"
   private_key_path = "D:\\ocicli\\keys\\sureshvenkey.pem"
 }
