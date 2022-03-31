6f3d90b8f50042d8930119ecef26e2af1ea1a77e
sureshvenkey <50266914+sureshvenkey@users.noreply.github.com>
2022-03-31 15:05:25 +0530

    Create provider.tf

---
+++ terraform/oci-core-instance/modules/provider.tf
@@ -0,0 +1,7 @@
+provider "oci" {
+  tenancy_ocid     = "ocid1.tenancy.oc1..aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"
+  user_ocid        = "ocid1.user.oc1..aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"
+  fingerprint      = "aa:bb:cc:21:dd:ee:a7:da:33:79:ee:e5:7e:rr:b7:23"
+  region           = "uk-london-1"
+  private_key_path = "D:\\ocicli\\keys\\sureshvenkey-03-16-01-29.pem"
+}
